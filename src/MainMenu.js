c266ede80ac99c8f6d77ff7c9c5f1db59c4fa2fc
Public Internet <Public@libnet.gvpl.ca>
2020-03-09 17:15:42 -0700

    tutorial

---
+++ src/MainMenu.js
@@ -0,0 +1,14 @@
+Cyber.MainMenu = function(game) {};
+Cyber.MainMenu.prototype = {
+	create: function() {
+		this.add.sprite(0, 0, 'screen-mainmenu');
+		this.gameTitle = this.add.sprite(Cyber._WIDTH*0.5, 40, 'title');
+		this.gameTitle.anchor.set(0.5,0);
+		this.startButton = this.add.button(Cyber._WIDTH*0.5, 200, 'button-start', this.startGame, this, 2, 0, 1);
+		this.startButton.anchor.set(0.5,0);
+		this.startButton.input.useHandCursor = true;
+	},
+	startGame: function() {
+		this.game.state.start('Howto');
+	}
+};
